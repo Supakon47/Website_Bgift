59d03ba724de8cc6ab659aa00cd8130d47109baf
supakon474 <supakon474@gmail.com>
2025-09-16 20:03:02 +0700

    semi-final func porariod for second page

--- birthday.js
+++ birthday.js
@@ -1,12 +1,19 @@
 const btnProfile = document.getElementById("btnProfile");
 const profileCard = document.getElementById("profileCard");
+const btnTwenty = document.getElementById("btnTwenty");
+const twentyCard = document.getElementById("twentyCard");
 
 btnProfile.addEventListener("click", () => {
   profileCard.classList.toggle("show");
   profileCard.classList.toggle("hidden");
 
-  // เลื่อน scroll ลงไปหาการ์ด เมื่อมันถูกเปิด
+  //  scroll Down to profileCard if it's shown
   if (profileCard.classList.contains("show")) {
     profileCard.scrollIntoView({ behavior: "smooth", block: "start" });
   }
+});
+
+btnTwenty.addEventListener("click", () => {
+  twentyCard.classList.add("show");
+  twentyCard.scrollIntoView({ behavior: "smooth", block: "start" });
 });
